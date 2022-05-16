6bf8235535fee675c88f3a58376125f4320cc752
Giovanna <giovannagmoeller@gmail.com>
2022-05-15 23:37:29 -0300

    refactor - separating responsabilities

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const checkboxInput = document.querySelector('input[type="checkbox"]')
 
 checkboxInput.addEventListener('change', checkCheckboxStatus)
 searchButton.addEventListener('click', searchMovie)
-input.addEventListener('keyup', function(event) {
+input.addEventListener('keydown', function(event) {
   if (event.keyCode == 13) { // Press enter (submit)
     searchMovie()
     return
@@ -17,22 +17,23 @@ input.addEventListener('keyup', function(event) {
 
 function checkCheckboxStatus() {
   const isChecked = checkboxInput.checked
+  cleanAllMovies()
   if (isChecked) {
-    cleanAllMovies()
     const movies = LocalStorage.getFavoriteMovies() || []
     movies.forEach(movie => renderMovie(movie))
   } else {
-    cleanAllMovies()
     getAllPopularMovies()
   }
 }
 
 async function searchMovie() {
   const inputValue = input.value
+  cleanAllMovies()
   if (inputValue != '') {
-    cleanAllMovies()
     const movies = await API.searchMovieByName(inputValue)
     movies.forEach(movie => renderMovie(movie))
+  } else {
+    getAllPopularMovies()
   }
 }
 
@@ -45,7 +46,6 @@ function favoriteButtonPressed(event, movie) {
     favorited: 'images/heart-fill.svg',
     notFavorited: 'images/heart.svg'
   }
-
   if(event.target.src.includes(favoriteState.notFavorited)) {
     // aqui ele será favoritado
     event.target.src = favoriteState.favorited
@@ -67,10 +67,9 @@ window.onload = function() {
 }
 
 function renderMovie(movie) {
-
   const { id, title, poster_path, vote_average, release_date, overview } = movie
-  const isFavorited = LocalStorage.checkMovieIsFavorited(id)
 
+  const isFavorited = LocalStorage.checkMovieIsFavorited(id)
   const year = new Date(release_date).getFullYear()
   const image = `https://image.tmdb.org/t/p/w500${poster_path}`
 
@@ -83,53 +82,66 @@ function renderMovie(movie) {
 
   const movieImageContainer = document.createElement('div')
   movieImageContainer.classList.add('movie-image')
+
   const movieImage = document.createElement('img')
   movieImage.src = image
   movieImage.alt = `${title} Poster`
+
   movieImageContainer.appendChild(movieImage)
   movieInformations.appendChild(movieImageContainer)
 
   const movieTextContainer = document.createElement('div')
   movieTextContainer.classList.add('movie-text')
+
   const movieTitle = document.createElement('h4')
   movieTitle.textContent = `${title} (${year})`
+
   movieTextContainer.appendChild(movieTitle)
   movieInformations.appendChild(movieTextContainer)
 
   const informations = document.createElement('div')
   informations.classList.add('movie-informations')
+
   movieTextContainer.appendChild(informations)
 
   const ratingContainer = document.createElement('div')
   ratingContainer.classList.add('rating')
+
   const starImage = document.createElement('img')
   starImage.src = 'images/star.png'
   starImage.alt = 'Star'
+
   const movieRate = document.createElement('span')
   movieRate.classList.add('movie-rate')
   movieRate.textContent = vote_average
+
   ratingContainer.appendChild(starImage)
   ratingContainer.appendChild(movieRate)
   informations.appendChild(ratingContainer)
 
   const favorite = document.createElement('div')
   favorite.classList.add('favorite')
+
   const favoriteImage = document.createElement('img')
   favoriteImage.src = isFavorited ? 'images/heart-fill.svg' : 'images/heart.svg'
   favoriteImage.alt = 'Heart'
   favoriteImage.classList.add('favoriteImage')
   favoriteImage.addEventListener('click', (event) => favoriteButtonPressed(event, movie))
+
   const favoriteText = document.createElement('span')
   favoriteText.classList.add('movie-favorite')
   favoriteText.textContent = 'Favoritar'
+
   favorite.appendChild(favoriteImage)
   favorite.appendChild(favoriteText)
   informations.appendChild(favorite)
 
   const movieDescriptionContainer = document.createElement('div')
   movieDescriptionContainer.classList.add('movie-description')
+
   const movieDescription = document.createElement('span')
   movieDescription.textContent = overview
+
   movieDescriptionContainer.appendChild(movieDescription)
   
   movieElement.appendChild(movieInformations)
